1e8dcbd94d92966c15578644b6e308a3a6cf5b18
LeylaBackend <leylaunusmuhamedova@gmail.com>
2023-12-14 14:21:01 +0600

    HW1

--- product/views.py
+++ product/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-
+from django.utils import timezone
 # Create your views here.
 
 def product_view(request):
@@ -11,8 +11,8 @@ def hello_view(request):
     return render(request, 'index.html')
 
 def current_date_view(request):
-    if request.method == 'GET':
-        return HttpResponse('13.12.2023')
+    current_date = timezone.now().date()
+    return HttpResponse(f'Current Date: {current_date}')
 
 def goodbye_view(request):
     if request.method == 'GET':
